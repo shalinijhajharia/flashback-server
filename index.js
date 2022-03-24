ad5625a2b1aea4620c8d912516f43f48d581aa6b
shalini jhajharia <shalinijhajharia185@gmail.com>
2022-03-24 12:04:50 +0530

    5665

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
+app.get('/',(req,res)=>{
+  res.send("hello to flashback API");
+});
+
 //const CONNECTION_URL=process.env.CONNECTION_URL;
 const PORT=process.env.PORT ||5000;
 
